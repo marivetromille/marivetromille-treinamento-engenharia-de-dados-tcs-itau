b1cda067b07bc63baf4d8500d8474aa44786cd4d
marivetromille <165916622+marivetromille@users.noreply.github.com>
2025-03-19 19:24:43 -0300

    Update Atividades.sql

--- 1. Consultas SQL Básicas e avançadas/Atividades.sql
+++ 1. Consultas SQL Básicas e avançadas/Atividades.sql
@@ -17,7 +17,7 @@ WHERE valor > 100
 
 SELECT *
 FROM Pedidos
-ORDER BY valor desc
+ORDER BY valor DESC
 
 
 
@@ -27,6 +27,18 @@ SELECT nome_produto
 FROM Produtos
 WHERE id_produto BETWEEN 1 AND 3
 
+OU
+
+SELECT *
+FROM Produtos
+ORDER BY id_produto
+LIMIT 3
+
+WHERE
+GROUP BY
+HAVING
+ORDER BY
+LIMIT
 
 
 /* Atividade 5 - Listar o total de valor gasto por cada cliente em pedidos */
@@ -36,6 +48,17 @@ FROM Pedidos p
 JOIN Clientes c ON p.id_cliente = c.id_cliente
 GROUP BY c.nome
 
+SELECT
+  c.id_cliente,
+  c.nome,
+  SUM(p.valor
+FROM
+  Clientes c
+  INNER JOIN Pedidos p
+     ON c.id_cliente = p.id_cliente
+GROUP BY
+  c.id_cliente,
+  c.nome
 
 
 /* Atividade 6 - Encontrar o cliente com o maior valor gasto */
@@ -44,6 +67,19 @@ SELECT MAX(p.valor) AS maior_valor, c.nome cliente_com_maior_valor_gasto
 FROM Pedidos p
 JOIN Clientes c ON p.id_cliente = c.id_cliente
 
+SELECT 
+   c.id_cliente,
+   c.nome,
+   SUM(p.valor)
+FROM
+   Clientes c
+   INNER JOIN Pedidos p
+      ON c.id_cliente = p.id_cliente
+GROUP BY
+   c.id_cliente,
+   c.nome
+ORDER BY valor_gasto DESC
+LIMIT 1
 
 
 /* Atividade 7 - Utilizar CTE para calcular o total de vendas por produto */
@@ -65,8 +101,26 @@ ON tot.id_pedido = it.id_pedido
 GROUP BY it.id_produto
 
 
+WITH VendasPorProduto AS (
+	SELECT
+	ip.id_produto,
+	SUM(ip.quantidade) total_vendido
+FROM ItensPedido ip
+GROUP BY ip.id_produto
+)
+SELECT 
+	p.id_produto, 
+	p.nome_produto, 
+	vp.total_vendido,
+	(vp.total_vendido * p.preco) valor_total_vendas
+FROM 
+	Produtos p
+	INNER JOIN VendasPorProduto vp
+	ON p.id_produto = vp.id_produto
 
-/* ATIVIDADE 8 - Listar todos os produtos comprados por cada cliente */
+
+
+/* Atividade 8 - Listar todos os produtos comprados por cada cliente */
 
 SELECT 
     c.nome AS nome_do_cliente,
@@ -81,8 +135,22 @@ GROUP BY
     c.id_cliente, c.nome, prod.id_produto, prod.nome_produto
 
 
+SELECT 
+    c.id_cliente,
+    c.nome,
+    p.id_produto,
+    p.nome_produto
+FROM 
+    Clientes c
+	INNER JOIN Pedidos pd 
+		ON c.id_cliente = pd.id_cliente
+	INNER JOIN ItensPedido ip 
+		ON pd.id_pedido = ip.id_pedido
+	INNER JOIN Produtos p 
+		ON p.id_produto = ip.id_produto
 
-/* ATIVIDADE 9 - Ranquear clientes pelo valor total gasto começando pelo rank 1 para o maior valor. */
+
+/* Atividade 9 - Ranquear clientes pelo valor total gasto começando pelo rank 1 para o maior valor. */
 
 SELECT c.nome, SUM(p.valor) AS total_gasto
 FROM Pedidos AS p
@@ -90,6 +158,19 @@ JOIN Clientes AS c ON p.id_cliente = c.id_cliente
 GROUP BY c.nome
 ORDER BY total_gasto desc
 
+
+SELECT 
+	c.id_cliente,
+	c.nome, 
+	SUM(p.valor) AS total_gasto,
+	RANK() OVER (ORDER BY SUM(p.valor) DESC) ranking
+FROM 
+	Clientes c
+	INNER JOIN Pedidos p
+		ON c.id_cliente = p.id_cliente
+GROUP BY 
+	c.id_cliente,
+	c.nome
 
 
 /* ATIVIDADE 10 - Número de pedidos por cliente, considerando apenas aqueles com mais de 1 pedido */
@@ -101,6 +182,19 @@ JOIN Clientes AS c ON ped.id_cliente = c.id_cliente
 WHERE it.quantidade > 1
 GROUP BY c.nome
 
+SELECT
+	c.id_cliente,
+	c.nome,
+	COUNT(p.id_pedido) quantidade_pedidos
+FROM
+	Clientes c
+	INNER JOIN Pedidos p
+		ON c.id_cliente = p.id_cliente
+GROUP BY
+	c.id_cliente,
+	c.nome
+HAVING
+	COUNT (p.id_pedido) > 1
 
 
 /* ATIVIDADE 11 - Calcular para cada cliente a quantidade de dias entre um pedido e o pedido imediatamente anterior
@@ -117,6 +211,25 @@ SELECT c.nome as cliente, pc.data_pedido AS data_atual, pc.data_ped_anterior AS 
   FROM Clientes c 
   JOIN ped_clientes pc ON c.id_cliente = pc.id_cliente
 
+
+WITH t AS (
+SELECT 
+	c.id_cliente,
+	c.nome,
+	p.id_pedido,
+	p.data_pedido,
+	LAG(p.data_pedido) OVER (PARTITION BY c.id_cliente ORDER BY p.data_pedido) AS data_pedido_anterior
+FROM 
+	Clientes c
+	INNER JOIN Pedidos p
+		ON c.id_cliente = p.id_cliente		
+)
+SELECT
+	*,
+	(JULIANDAY(data_pedido) - JULIANDAY(data_pedido_anterior)) dias_entre_pedidos
+FROM t
+
+
 /* ATIVIDADE 12
 Crie uma consulta que retorne um relatórios contendo as seguintes colunas (obs: use o padrão que preferir para nomear as colunas):
 ID do cliente
@@ -129,45 +242,31 @@ Preço do pedido sem desconto (pode ser recuperado somando a coluna "preço" de 
 Quantidade de dias entre o pedido e seu pedido imediatamente anterior
  */
 
-WITH PedidosComPrecoTotal AS (
-    SELECT 
-        p.ID_CLIENTE,
-        p.ID_PEDIDO,
-        p.DATA_PEDIDO,
-        SUM(ip.QUANTIDADE * pr.PRECO) AS VALOR_PEDIDO_SEM_DESCONTO,
-        p.VALOR_PEDIDO
-    FROM 
-        Pedidos p
-    JOIN 
-        ItensDoPedido ip ON p.ID_PEDIDO = ip.ID_PEDIDO
-    JOIN 
-        Produtos pr ON ip.ID_PRODUTO = pr.ID_PRODUTO
-    GROUP BY 
-        p.ID_CLIENTE, p.ID_PEDIDO, p.DATA_PEDIDO, p.VALOR_PEDIDO
-),
-PedidosComDiasAtras AS (
-    SELECT 
-        ID_CLIENTE,
-        ID_PEDIDO,
-        DATA_PEDIDO,
-        VALOR_PEDIDO_SEM_DESCONTO,
-        VALOR_PEDIDO,
-        LAG(DATA_PEDIDO) OVER (PARTITION BY ID_CLIENTE ORDER BY DATA_PEDIDO) AS DATA_PEDIDO_ANTERIOR
-    FROM 
-        PedidosComPrecoTotal
+WITH ValorSemDesconto AS (
+	SELECT
+	ip.id_pedido,
+	SUM(p.preco * ip.quantidade) preco_sem_desconto
+FROM
+	ItensPedido ip
+	INNER JOIN Produtos p
+		ON ip.id_produto = p.id_produto
+GROUP BY 
+	ip.id_pedido
 )
-SELECT 
-    c.ID_CLIENTE,
-    c.NOME AS NOME_CLIENTE,
-    c.CIDADE,
-    p.ID_PEDIDO,
-    p.DATA_PEDIDO,
-    p.VALOR_PEDIDO,
-    p.VALOR_PEDIDO_SEM_DESCONTO,
-    DATEDIFF(p.DATA_PEDIDO, p.DATA_PEDIDO_ANTERIOR) AS DIAS_ENTRE_PEDIDOS
-FROM 
-    PedidosComDiasAtras p
-JOIN 
-    Clientes c ON p.ID_CLIENTE = c.ID_CLIENTE
-WHERE 
-    p.DATA_PEDIDO_ANTERIOR IS NOT NULL;
+SELECT
+c.id_cliente		ID_CLIENTE,
+c.nome				NOME_CLIENTE,
+c.cidade			CIDADE_CLIENTE,
+p.id_pedido			ID_PEDIDO,
+p.data_pedido		DATA_PEDIDO,
+p.valor				VALOR_PEDIDO,
+vsd.preco_sem_desconto	VALOR_PEDIDO_SEM_DESCONTO
+	JULIANDAY(p.data_pedido) - JULIANDAY(
+	LAG(p.data_pedido)
+	OVER (PARTITION BY c.id_cliente ORDER BY p.data_pedido)
+	) DIAS_DESDE_PEDIDO_ANTERIOR
+FROM
+	Clientes c
+	INNER JOIN Pedidos p
+		ON c.id_cliente = p.id_cliente
+	LEFT JOIN ValorSemDesconto vsd
